50386139b53ababb5f0b4ff4ed30b4d723d27354
jean <jeanct0310@gmail.com>
2024-09-15 09:40:34 -0500

    primer commit

---
+++ src/main/java/com/cibertec/sesion1/Semana01Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.sesion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/sesion1/Semana01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.sesion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
